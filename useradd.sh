1a59f3bca225388b12bd09c529db8e9f0677bdc9
cloud_user <cloud_user@mylabserver.com>
2022-12-01 19:43:27 +0000

    Added # marks for note lines

--- useradd.sh
+++ useradd.sh
@@ -2,7 +2,7 @@
 # Script used to auto add a user to the server by: Tait Hoglund
 
 # This if statement first checks that I am Root User, then proceeds to ask the user for a username & pasword. 
-It then searches existing database to ensure there is no username already in existance.
+# It then searches existing database to ensure there is no username already in existance.
 
 if [ $(id -u) -eq 0 ]; then
         read -p "Select a Username : " username
@@ -10,8 +10,8 @@ if [ $(id -u) -eq 0 ]; then
         egrep "^$username" /etc/passwd > /dev/null
 
 # If a user name is found, it prompts the user to pick another name. If the name is available, it then asks the user
-to pick a password and assigns it to that user and uses the perl command below to encrypt the password into
-etc/shadow, then adds the user and pass word to the system
+# to pick a password and assigns it to that user and uses the perl command below to encrypt the password into
+# etc/shadow, then adds the user and pass word to the system
 
                 if [ $? -eq 0 ]; then
                                 echo "$username already exists. Please choose a different username."
